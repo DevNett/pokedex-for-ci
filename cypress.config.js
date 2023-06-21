9c164d285123117d357b2299f22ae177167fe121
root <luis.montes@webmaps.com.mx>
2023-06-21 16:22:52 -0600

    Cypress Deployment

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  video: true,
+  video: false,
   e2e: {
     supportFile: false,
     baseUrl: 'http://localhost:5000',
